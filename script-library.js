fa29c65aa8b56e24a792394f1f4a37e7f1ebcd4f
Sam Reedy <samuel.reedy@hotmail.com>
2021-09-21 07:50:29 +1000

    added prototyping to put a block on the page when form is executed

--- script-library.js
+++ script-library.js
@@ -1,27 +1,52 @@
 let myLibrary = [];
 
+//puts the user input into an object in myLIbrary
 const addBook = (event) => {
-    event.preventDefault(); //stop form submitting
-    let book = {
-        title : document.getElementById("title").value,
-        author : document.getElementById("author").value,
-        length : document.getElementById("length").value, 
-        read : document.getElementsByClassName("read").value
-    
+
+   event.preventDefault(); //stop form submitting
+
+    let Book = function() {
+        this.title = document.getElementById("title").value,
+        this.author = document.getElementById("author").value,
+        this.length = document.getElementById("length").value,
+        this.read = readStatus(event)
     }
-    myLibrary.push(book);
-    console.log(myLibrary[0]);
+
+    //creates new book tile
+    Book.prototype.addTile = function(){
+            const bookSection = document.querySelector(".book-section");
+            const bookContainer = document.createElement("div");
+            bookContainer.classList.add("book-container");
+            bookSection.appendChild(bookContainer);        
+    }
+
+    myLibrary = new Book();
+
+    myLibrary.addTile();
+
+    console.log(myLibrary);
+    document.querySelector(".user-input").reset();
+    
 };
 
 document.getElementById('submit').addEventListener('click', addBook);
 
-/* function Book(title, author, length, status){
-    this.title = title,
-    this.author = author
-    this.length = length,
-    this.status = status
+
+
+
+
+
+
+
+
+
+
+//stops NULL if no read status selected - NEEDS TO BE FIXED
+function readStatus(){
+    let readChecked = document.querySelector('input[class="read"]:checked').value
+    if (readChecked == null) {
+        return readChecked = "";
+    } else {
+        return readChecked;
+    }
 }
-
-//myLibrary = new Book("Harry Potter", "JK ROwling", "300", "READ")
-
-*/
