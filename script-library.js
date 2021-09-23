a008119ee17a86bba73324d860cb9c15cacc276d
Sam Reedy <samuel.reedy@hotmail.com>
2021-09-23 20:11:14 +1000

    send the block sideways, need to add buttons and UX

--- script-library.js
+++ script-library.js
@@ -13,11 +13,8 @@ const addBook = (event) => {
     }
 
     //creates new book tile
-    Book.prototype.addTile = function(){
-            const bookSection = document.querySelector(".book-section");
-            const bookContainer = document.createElement("div");
-            bookContainer.classList.add("book-container");
-            bookSection.appendChild(bookContainer);        
+    Book.prototype.addTile = () => {
+        domCreateTile(event);
     }
 
     myLibrary = new Book();
@@ -32,7 +29,36 @@ const addBook = (event) => {
 document.getElementById('submit').addEventListener('click', addBook);
 
 
+function domCreateTile() {
+    const bookSection = document.querySelector(".book-section");
+    const bookContainer = document.createElement("div");
+    bookContainer.classList.add("book-container");
+    bookSection.appendChild(bookContainer);
+    
+    const bookContent = document.createElement("ul");
+    bookContent.classList.add("book-content");
+    bookContainer.appendChild(bookContent);
 
+    let bookInfo = document.createElement("li");
+    bookInfo.classList.add("book-info");
+    bookInfo.textContent = myLibrary.title;
+    bookContent.appendChild(bookInfo);
+
+    bookInfo = document.createElement("li");
+    bookInfo.classList.add("book-info");
+    bookInfo.textContent = myLibrary.author;
+    bookContent.appendChild(bookInfo);
+
+    bookInfo = document.createElement("li");
+    bookInfo.classList.add("book-info");
+    bookInfo.textContent = myLibrary.length;
+    bookContent.appendChild(bookInfo);
+
+    bookInfo = document.createElement("li");
+    bookInfo.classList.add("book-info");
+    bookInfo.textContent = myLibrary.read;
+    bookContent.appendChild(bookInfo);
+    }
 
 
 
@@ -50,3 +76,4 @@ function readStatus(){
         return readChecked;
     }
 }
+
